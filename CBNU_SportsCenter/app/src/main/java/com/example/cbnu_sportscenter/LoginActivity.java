6efe0f7fb741429b84c987a607ca2444c2bb34fb
the-groot <sangwoo0795@naver.com>
2022-05-01 00:56:40 +0900

    회원가입 버튼클릭시 회원가입화면으로 전환

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/LoginActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.cbnu_sportscenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button2=findViewById(R.id.button2);
+
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
