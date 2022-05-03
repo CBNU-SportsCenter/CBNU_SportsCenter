180b222fe73cfddd89b52d73102e3360d9b97dc8
the-groot <sangwoo0795@naver.com>
2022-05-03 23:14:39 +0900

    회원가입 화면 구현완료

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/SignupActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/SignupActivity.java
@@ -18,9 +18,21 @@ import org.json.JSONObject;
 
 public class SignupActivity extends AppCompatActivity {
 
+    Button btn_back;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        btn_back=findViewById(R.id.btn_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
