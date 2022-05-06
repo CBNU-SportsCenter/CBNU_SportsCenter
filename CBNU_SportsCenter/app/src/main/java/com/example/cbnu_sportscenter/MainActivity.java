63a945c42775fc4fe84f7599e0dfa9eddce648a3
maatanyy <maatanyy@naver.com>
2022-05-07 00:42:28 +0900

    UserCertificate Activity 추가, 1차 qr code 화면 완성

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/MainActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.cbnu_sportscenter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/UserCertificate.java
@@ -0,0 +1,30 @@
+package com.example.cbnu_sportscenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class UserCertificate extends AppCompatActivity {
+
+    TextView userMajor, studentCode, userName, currentTime, university;
+    ImageView profileImage, reNew, qrCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_certificate);
+
+        userMajor = findViewById(R.id.userMajor);
+        studentCode = findViewById(R.id.studentCode);
+        userName = findViewById(R.id.userName);
+        currentTime = findViewById(R.id.currentTime);
+        university = findViewById(R.id.university);
+
+        profileImage = (ImageView)findViewById(R.id.profileImage);
+        reNew = (ImageView)findViewById(R.id.reNew);
+        qrCode = (ImageView)findViewById(R.id.qrCode);
+
+    }
+}
