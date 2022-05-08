418f157cfb31eb8f5047f5084c9326fc3bd76bf9
iingy <Evergyu@github.com>
2022-05-08 21:01:15 +0900

    Login to Main

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/LoginActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/LoginActivity.java
@@ -6,29 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btn_signup;
+    Button btn_signup,btn_login;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btn_signup=findViewById(R.id.btn_signup);
+        btn_signup=(Button) findViewById(R.id.btn_signup);
 
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login=findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MainPageActivity.class);
                 startActivity(intent);
             }
         });
--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/MainPageActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/MainPageActivity.java
@@ -1,26 +1,16 @@
 package com.example.cbnu_sportscenter;
 
-
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Fragment;
 
 public class MainPageActivity extends AppCompatActivity {
-    Button btnCer,btnUsage,btnIntro,btnNotice;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnCer.findViewById(R.id.Cert);
-        btnUsage.findViewById(R.id.Usag);
-        btnIntro.findViewById(R.id.Intr);
-        btnNotice.findViewById(R.id.Noti);
-        
-
     }
 }
