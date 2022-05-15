b34dc4a8d1eb596b208f455b4556ebad729f50eb
maatanyy <maatanyy@naver.com>
2022-05-16 00:46:16 +0900

    viewFlipper 추가->소개페이지 완성, 내부 시설 및 오시는 길 페이지 제작전

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/IntroductionActivity2.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/IntroductionActivity2.java
@@ -3,10 +3,11 @@ package com.example.cbnu_sportscenter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class IntroductionActivity2 extends AppCompatActivity {
 
-    ImageView centerImage;
+    ViewFlipper viewFlipper;
 
 
     @Override
@@ -14,7 +15,25 @@ public class IntroductionActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction2);
 
-        centerImage = findViewById(R.id.centerimage);
+        viewFlipper = findViewById(R.id.viewFlipper);
+
+
+        int images[]={ R.drawable.sportscenter, R.drawable.sportscenter2, R.drawable.sportscenter3};
+
+        for(int image:images){
+            viewflipperimages(image);
+        }
+    }
+
+    public void viewflipperimages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+        viewFlipper.addView(imageView);     //이미지추가
+        viewFlipper.setFlipInterval(3000);  //3초마다
+        viewFlipper.setAutoStart(true);     //처음 시작 여부
+
+        viewFlipper.setInAnimation(this,android.R.anim.slide_in_left);  //animation
+        viewFlipper.setOutAnimation(this,android.R.anim.slide_out_right);
 
     }
 }
