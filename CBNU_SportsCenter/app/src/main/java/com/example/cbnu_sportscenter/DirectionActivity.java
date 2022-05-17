486f930c0b469af16f9011ff453685e81bb3fff3
maatanyy <maatanyy@naver.com>
2022-05-17 19:55:57 +0900

    google 지도 어지러운 오류

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/DirectionActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/DirectionActivity.java
@@ -1,14 +1,44 @@
 package com.example.cbnu_sportscenter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class DirectionActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DirectionActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direction);
+
+        fragmentManager = getFragmentManager();
+        mapFragment =(MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        LatLng location = new LatLng(35.11,129.54);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("스포츠 센터");
+        markerOptions.snippet("스포츠센터 입니다");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
     }
 }
