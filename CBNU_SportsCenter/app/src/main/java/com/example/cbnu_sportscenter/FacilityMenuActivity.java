10506d8748aa6db6df406df8c474db795ed8e026
maatanyy <maatanyy@naver.com>
2022-05-30 18:13:42 +0900

    각 층 연결 및 안내도 1차 마무리

--- CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/FacilityMenuActivity.java
+++ CBNU_SportsCenter/app/src/main/java/com/example/cbnu_sportscenter/FacilityMenuActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FacilityMenuActivity extends AppCompatActivity {
 
     TextView toFloor0, toFloor1, toFloor2, toFloor3, toFloor4;
+    ImageView directionImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,38 +24,39 @@ public class FacilityMenuActivity extends AppCompatActivity {
         toFloor3 = findViewById(R.id.toFloor3);
         toFloor4 = findViewById(R.id.toFloor4);
 
+        directionImage = findViewById(R.id.directionImage);
         toFloor0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                directionImage.setImageResource(R.drawable.fl0);
             }
         });
 
         toFloor1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                directionImage.setImageResource(R.drawable.fl1);
             }
         });
 
         toFloor2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                directionImage.setImageResource(R.drawable.fl2);
             }
         });
 
         toFloor3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                directionImage.setImageResource(R.drawable.fl3);
             }
         });
 
         toFloor4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                directionImage.setImageResource(R.drawable.fl4);
             }
         });
 
